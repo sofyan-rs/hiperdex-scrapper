5fcd232fb3b2dd100b486c5f4eabfb3e3661c8ab
Sofyan R <sofyan423@gmail.com>
2021-12-22 21:30:33 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -36,7 +36,6 @@ app.get('/api/info/:slug', async (req, res) => {
 
 app.get('/api/chapter/:manga/:chapter', async (req, res) => {
     const result = await scapper.chapter(req.params.manga,req.params.chapter)
-    res.setHeader('Cache-Control', 's-maxage=43200');
     res.header("Content-Type", 'application/json');
     res.send(JSON.stringify(result, null, 4))
 })
