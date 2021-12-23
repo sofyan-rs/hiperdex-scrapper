ea7113a109b5f415220b0666f21ac88e9a64d93a
Sofyan R <sofyan423@gmail.com>
2021-12-23 09:52:51 +0700

    Update scrapper.js

--- api/scrapper.js
+++ api/scrapper.js
@@ -131,7 +131,8 @@ async function all(page) {
         
         let last_page = $('.last').attr('href')
 	
-	let test_page = $('.pages').text().match(/\d+/g)
+	let test_page = $('.pages').text()
+	let test_lagi = test_page.match(/\d+/g)
 
         
 	!last_page?last_page=current:last_page
@@ -141,7 +142,7 @@ async function all(page) {
             'list': m_list,
             'current_page': parseInt(current),
             'last_page': parseInt(last_page.replace(/[^0-9]/g, '')),
-	    'test_page': test_page,
+	    'test_page': test_lagi,
         })
     } catch (error) {
         return await ({'error': 'Sorry dude, an error occured! No Latest!'})
